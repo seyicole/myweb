259052d4e31c3b082cf0a4185fda080c8bc695a1
sey cole <seyicole>
2021-07-13 21:04:06 +0100

    minor changes

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -9,15 +9,16 @@ export default function Resume() {
             <p>Freelance developer – present</p>
             <p>Full-stack Developer - Full Stack (HTML, CSS, JavaScript, Express, React)</p>
 
-            <p><a href='https://www.syagem.com/' target="blank"><span>Syagem Media</span></a> June 2019 – January, 2020</p>
+            <p><a href='https://swipemax.com/' target="blank"><span>Swipemax</span></a> June 2019 – January, 2020</p>
             <p>Frontend developer - Front-end (HTML, CSS, React, Wordpress)</p>
 
-            <p><a href='https://www.amplify.ng/' target='blank'><span> Amplify Digital Agency</span></a> March 2018 – April 2019</p>
+            {/* <p><a href='https://www.amplify.ng/' target='blank'><span> Amplify Digital Agency</span></a> March 2018 – April 2019</p>
             <p>Lead Developer - Front-end (HTML, CSS, Javascript, Node)</p>
 
             <p><a href='https://swipemax.com/' target="blank"><span>Swipemax</span></a> 2017 – 2018</p>
-            <p>Frontend Developer - Front-end (HTML, CSS, jQuery, JavaScript)</p>
+            <p>Frontend Developer - Front-end (HTML, CSS, jQuery, JavaScript)</p> */}
             
         </div>
     )
 }
+
